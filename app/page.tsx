e62d9772b9be1ac617fb4914dab33e1d84f933e5
Tyler Haisman <tyler@haisman.net>
2025-11-02 20:08:30 -0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,50 @@
+"use client";
+
 import Image from "next/image";
+import HappyBirthday from "../public/happy_birthday.png";
+import Alyssa from "../public/20241229_002839073_iOS.png";
+import { useState } from "react";
+import Confetti from "react-confetti";
 
 export default function Home() {
+  const [showImage, setShowImage] = useState(false);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="relative overflow-hidden flex flex-col justify-center items-center w-full bg-linear-to-br from-purple-300 to-purple-500 text-white min-h-screen">
+      <div className="relative h-52 sm:h-screen flex flex-col justify-center items-center w-full">
+        <Image src={HappyBirthday} alt="" className="w-screen"></Image>
+        <h2 className="text-[26vw] text-pink-300 z-20 absolute">Alyssa</h2>
+      </div>
+      <button
+        className="absolute bottom-4 left-0 right-0 text-xs bg-white rounded-full text-purple-500 w-fit px-2 py-1 mx-auto cursor-pointer hover:scale-105 duration-100"
+        onClick={() => setShowImage(true)}
+      >
+        click me
+      </button>
+      <div className="z-30" style={{ display: showImage ? "block" : "none" }}>
+        <Confetti
+          className="z-40"
+          gravity={0.3}
+          recycle
+          width={window.innerWidth}
+          height={window.innerHeight}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+      {showImage && (
+        <button
+          className="z-30 fixed top-4 right-4 text-xs bg-white rounded-full text-purple-500 w-fit px-2 py-1 mx-auto cursor-pointer hover:scale-105 duration-100"
+          onClick={() => setShowImage(false)}
+        >
+          close
+        </button>
+      )}
+      {showImage && (
+        <Image
+          src={Alyssa}
+          alt=""
+          className="fixed left-0 top-0 h-screen w-screen z-20"
+        ></Image>
+      )}
     </div>
   );
 }
